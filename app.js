0876e67e112c90668216f8b690bad36d1a461dd0
Raphael Morineau <raphael.morineau@gmail.com>
2019-05-06 14:47:12 +0200

    Add links to Readme

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// interesting sources
-// Main ThreeJS documentation: https://threejs.org/docs
-// Mouse controls with dragging and axes: https://threejs.org/examples/misc_controls_transform.html
-// Fluid orientation changes: https://threejs.org/examples/#webgl_math_orientation_transform
-// wireframe: https://threejs.org/examples/#webgl_geometry_convex
-// wireframe: https://threejs.org/examples/#webgl_lines_fat_wireframe
-// https://threejs.org/examples/?q=raycast#webgl_raycast_texture
-// https://threejs.org/examples/#webgl_interactive_buffergeometry
-// add an object: https://threejs.org/examples/#webgl_interactive_voxelpainter
-// precision using nb faces: https://threejs.org/examples/#webgl_modifier_subdivision
-// comparison to show defects : https://threejs.org/examples/#webgl_multiple_scenes_comparison
-// https://threejs.org/examples/#webgl_nearestneighbour
-// Explanations why can not color faces with BufferGeometries:
-// https://stackoverflow.com/questions/41670308/three-buffergeometry-how-do-i-manually-set-face-colors#answer-41682130
-// Docs BufferAttribute: https://threejs.org/docs/index.html#api/en/core/BufferAttribute
-// Color on click: https://stackoverflow.com/questions/50118025/changing-color-of-points-in-a-particle-system-dynamically#answer-50118263
-// color multiple faces with dragging mouse: https://jsfiddle.net/Shaggisu/w7ufmutr/9/
-
 const model1 = '96016204_000_light.stl';
 const model2 = 'https://threejs.org/examples/models/stl/ascii/slotted_disk.stl';
 const props = {
